6ec7b076a44e8d96b11ec47cca188883f1673a75
nicolas capalbo <capalbo.nicolas@gmail.com>
2020-07-22 16:26:42 -0300

    feed backend, frontend structure and css adjustments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # raise
+    @tweet = Tweet.new
+    @user = current_user
+    if current_user.nil?
+      @tweets = Tweet.all.order("created_at")
+    else
+      @tweets = []
+      current_user.followers.each do |follower|
+        follower.tweets.each do |tweet|
+          @tweets << tweet
+        end
+      end
+      @tweets = @tweets.sort_by { |tweet| tweet.created_at}.reverse!
+    end
   end
 
 end
