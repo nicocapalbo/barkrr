e129c331acb8599318404e14db883b71bf665140
nicolas capalbo <capalbo.nicolas@gmail.com>
2020-07-27 10:58:16 -0300

    schema/seed fix; followings uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2020_07_20_153758) do
     t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id", "follower_id"], name: "index_followings_on_followed_id_and_follower_id", unique: true
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
   end
 
   create_table "tweets", force: :cascade do |t|
